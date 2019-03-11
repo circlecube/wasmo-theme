b6f1c2b723dc735bf0df6e874d98be866d340174
Evan Mullins <evan.mullins@get10up.com>
2019-03-11 15:53:42 -0600

    footer note re:church

--- footer.php
+++ footer.php
@@ -22,7 +22,7 @@
 			<?php if ( ! empty( $blog_info ) ) : ?>
 				<a class="site-name" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>,
 			<?php endif; ?>
-			
+			This site is not affiliated or managed by The Church of Jesus Christ of Latter Day Saints. They don't want to be called mormon anymore anyways.
 			<?php
 			if ( function_exists( 'the_privacy_policy_link' ) ) {
 				the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
