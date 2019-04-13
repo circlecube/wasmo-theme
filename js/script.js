480044b1907fff38aa8dd5a1f333f1e9a72dae60
Evan Mullins <evan.mullins@get10up.com>
2019-04-13 14:12:56 -0600

    add link handler to open register modal

--- js/script.js
+++ js/script.js
@@ -5,7 +5,14 @@ jQuery(document).ready(function($) {
 	// open modal on nav link click
 	function navLoginClick(e) {
 		e.preventDefault();
+		// open login modal
 		$('.lwa-links-modal').trigger('click');
 	}
+
+	$('.register').on('click', function(e){ 
+		e.preventDefault(); 
+		// open register modal
+		$('.lwa-links-register').trigger('click'); 
+	} );
 	
 });
