407f4fb7cd56e2521cbf083e2364d9423829df99
Evan Mullins <evan.mullins@get10up.com>
2020-05-12 14:13:39 -0400

    update footer disclaimer and add acf field to content manage

--- footer.php
+++ footer.php
@@ -20,7 +20,11 @@
 			<?php if ( ! empty( $blog_info ) ) : ?>
 				<a class="site-name" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
 			<?php endif; ?>
-			This site is not affiliated or managed by The Church of Jesus Christ of Latter Day Saints. They don't want to be called mormon anymore anyways.
+			<?php if ( get_field( 'legal_disclaimer', 'option' ) ) { ?>
+				<p class="legal_disclaimer"><?php the_field( 'legal_disclaimer', 'option' ); ?></p>
+			<?php } else { ?>
+				<p class="legal_disclaimer">Though this site discusses mormonism, topics related to mormons, the mormon church and people who refer to themselves as unorthodox mormons, ex-mormons, post-mormons or any other form of wasmormon, it is not officially affiliated with or managed by The Church of Jesus Christ of Latter-day Saints or even the Corporation of the Presiding Bishop. They don't want to be called mormon anymore anyways. All of the content, stories or opinions expressed, implied or included in this site are solely credited to those sharing their own personal stories and not those of Intellectual Reserve, Inc. or The Church of Jesus Christ of Latter-day Saints.</p>
+			<?php } ?>
 			<?php
 			if ( function_exists( 'the_privacy_policy_link' ) ) {
 				the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
