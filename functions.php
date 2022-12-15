6195a69bb531ab6934541b441b2cb28192478a47
Evan Mullins <evanrm@gmail.com>
2022-12-15 15:18:35 -0500

    customize seo and robots functions

--- functions.php
+++ functions.php
@@ -112,7 +112,7 @@ function cptui_register_my_taxes() {
 		"publicly_queryable" => true,
 		"hierarchical" => false,
 		"show_ui" => true,
-		"show_in_menu" => false,
+		"show_in_menu" => true,
 		"show_in_nav_menus" => false,
 		"query_var" => true,
 		"rewrite" => array( 'slug' => 'spectrum', 'with_front' => true, ),
@@ -121,7 +121,7 @@ function cptui_register_my_taxes() {
 		"rest_base" => "spectrum",
 		"rest_controller_class" => "WP_REST_Terms_Controller",
 		"show_in_quick_edit" => false,
-		);
+	);
 	register_taxonomy( "spectrum", array( "post", "user" ), $args );
 
 	/**
@@ -140,7 +140,7 @@ function cptui_register_my_taxes() {
 		"publicly_queryable" => true,
 		"hierarchical" => false,
 		"show_ui" => true,
-		"show_in_menu" => false,
+		"show_in_menu" => true,
 		"show_in_nav_menus" => false,
 		"query_var" => true,
 		"rewrite" => array( 'slug' => 'shelf', 'with_front' => true, ),
@@ -149,7 +149,7 @@ function cptui_register_my_taxes() {
 		"rest_base" => "shelf",
 		"rest_controller_class" => "WP_REST_Terms_Controller",
 		"show_in_quick_edit" => false,
-		);
+	);
 	register_taxonomy( "shelf", array( "post", "user" ), $args );
 }
 add_action( 'init', 'cptui_register_my_taxes' );
@@ -412,12 +412,13 @@ function wasmo_update_user( $post_id ) {
 		'info'
 	);
 
+	// update last_save timestamp for this user
+	update_user_meta( $user_id, 'last_save', time() );
+
 	//only if not edited by an admin
 	if ( !current_user_can( 'administrator' ) ) {
 		// notify email
 		wasmo_send_admin_email__profile_update( $user_id, $save_count );
-		// update last_save timestamp for this user
-		update_user_meta( $user_id, 'last_save', time() );
 		/*
 		// if admin - check if welcome email has been sent.
 		$has_received_welcome = get_user_meta( $user_id, 'has_received_welcome', true );
@@ -1230,3 +1231,98 @@ function wasmo_fix_post_counts($views) {
 	}
 	return $views;
 }
+
+/**
+ * Allow author pages to be indexed, unless unlisted
+ */
+add_filter( 'wp_robots', function( $robots ) {
+	if ( is_author() ) {
+		$curauth = ( get_query_var('author_name') ) ? get_user_by( 'slug', get_query_var( 'author_name' ) ) : get_userdata( get_query_var( 'author' ) );
+		$userid = $curauth->ID;
+
+		// check privacy setting, noindex if not public
+		$in_directory = get_field( 'in_directory', 'user_' . $userid );
+		if(
+			!$in_directory ||
+			'false' === $in_directory ||
+			'private' === $in_directory
+		) {
+			$robots['noindex'] = true;
+		} else {
+			// otherwise, index
+			$robots['noindex'] = false;
+		}
+	}
+	return $robots;
+});
+
+/**
+ * Filter authors in sitemap to include all who are public and have required fields
+ */
+add_filter('aioseo_sitemap_author_archives', function( $entries ) {
+	$authors = get_users();
+	$entries = [];
+	foreach ( $authors as $author ) {
+		// check that profile has required content
+		// require both hi and tagline content, bail early if not present
+		if (
+			!get_field( 'hi', 'user_' . $author->ID ) ||
+			!get_field( 'tagline', 'user_' . $author->ID )
+		) {
+			continue;
+		}
+
+		// check privacy setting, bail if not public
+		$in_directory = get_field( 'in_directory', 'user_' . $author->ID );
+		if(
+			!$in_directory ||
+			'false' === $in_directory ||
+			'private' === $in_directory
+		) {
+			continue;
+		}
+
+		// get the last save time
+		$last_save = intval( get_user_meta( $author->ID, 'last_save', true ) );
+
+
+		// $user = get_userdata( $author->ID );
+		// $registered = strtotime( $user->user_registered );
+		$entries[] = [
+			'loc'        => get_author_posts_url( $author->ID ),
+			'lastmod'    => $last_save ? date('Y-m-d H:i:s', $last_save ) : false,
+			'changefreq' => aioseo()->sitemap->priority->frequency( 'author' ),
+			'priority'   => aioseo()->sitemap->priority->priority( 'author' ),
+		];
+	}
+	return $entries;
+});
+
+/**
+ * Filter aioseo description for profile pages with acf custom fields
+ */
+add_filter( 'aioseo_description', function ( $description ) {
+	if ( is_author() ) {
+		$curauth = ( get_query_var('author_name') ) ? get_user_by( 'slug', get_query_var( 'author_name' ) ) : get_userdata( get_query_var( 'author' ) );
+		$userid = $curauth->ID;
+    	$description .= get_field( 'hi', 'user_' . $userid ) . '. ' . get_field( 'tagline', 'user_' . $userid );
+	}
+	return $description;
+});
+
+/**
+ * Add og meta values for author pages
+ */
+add_action( 'wp_head', function () {
+	if ( is_author() ) {
+		$curauth = ( get_query_var('author_name') ) ? get_user_by( 'slug', get_query_var( 'author_name' ) ) : get_userdata( get_query_var( 'author' ) );
+		$userid = $curauth->ID;
+		?>
+		<meta property="og:title" content="I'm <?php echo $curauth->display_name; ?> and I was a mormon. A wasmormon.org profile." />
+		<meta property="og:description" content="<?php echo get_field( 'hi', 'user_' . $userid ); ?>. <?php echo get_field( 'tagline', 'user_' . $userid ); ?> Read my story to learn why I left the mormon church." />
+		<meta property="og:type" content="profile" />
+		<meta property="og:site_name" content="wasmormon.org" />
+		<meta property="og:url" content="<?php echo get_author_posts_url( $userid ); ?>" />
+		<?php
+	}
+});
