29a1efad6f0e6e6b76c03b6b69ad71067058e329
Evan Mullins <evanrm@gmail.com>
2023-01-30 10:36:36 -0500

    spotlight pattern cleanup

--- patterns/profile-spotlight.php
+++ patterns/profile-spotlight.php
@@ -7,7 +7,7 @@
 
 ?><!-- wp:group {"layout":{"type":"constrained"}} -->
 <div class="wp-block-group"><!-- wp:paragraph -->
-<p>Intro</p>
+<p>NAME Was a Mormon, an Ex-Mormon Profile Spotlight</p>
 <!-- /wp:paragraph -->
 
 <!-- wp:gallery {"columns":2,"imageCrop":false,"linkTo":"none"} -->
@@ -17,17 +17,17 @@
 
 <!-- wp:image {"id":1511,"sizeSlug":"large","linkDestination":"none"} -->
 <figure class="wp-block-image size-large"><img src="https://wasmormon.org/wp-content/uploads/sites/12/2022/11/wasmo-fbb-2-1024x1024.jpg" alt="" class="wp-image-1511"/></figure>
-<!-- /wp:image --><figcaption class="blocks-gallery-caption wp-element-caption">Caption - <a href="https://wasmormon.org?randomprofile=1">Profile</a></figcaption></figure>
+<!-- /wp:image --><figcaption class="blocks-gallery-caption wp-element-caption">Caption - <a href="#">Profile</a></figcaption></figure>
 <!-- /wp:gallery -->
 
 <!-- wp:quote -->
 <blockquote class="wp-block-quote"><!-- wp:paragraph -->
-<p>"Quote"</p>
-<!-- /wp:paragraph --><cite><a href="https://wasmormon.org/?randomprofile=1">Profile</a></cite></blockquote>
+<p>Quote</p>
+<!-- /wp:paragraph --><cite><a href="#">Profile</a></cite></blockquote>
 <!-- /wp:quote -->
 
 <!-- wp:paragraph -->
-<p>Continue reading the full wasmormon profile at <a href="https://wasmormon.org/?randomprofile=1">https://wasmormon.org?randomprofile=1</a></p>
+<p>Continue reading the full wasmormon profile at <a href="#">https://wasmormon.org/profile/</a></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:separator -->
@@ -36,7 +36,7 @@
 
 <!-- wp:group {"layout":{"type":"constrained"},"fontSize":"small"} -->
 <div class="wp-block-group has-small-font-size"><!-- wp:paragraph {"fontSize":"small"} -->
-<p class="has-small-font-size">Discuss this spotlight in the comments or on your favorite social media: <a href="#" rel="nofollow">instagram</a>, <a href="#" rel="nofollow">facebook</a>, <a href="#" rel="nofollow">reddit</a>, <a href="#" target="_blank" rel="noopener nofollow" title="">pinterest</a>, <a href="#" target="_blank" rel="noopener nofollow" title="">twitter</a>, <a href="#" target="_blank" rel="noopener nofollow" title="">mastodon</a>, <a href="#" target="_blank" rel="noopener nofollow" title="">tumblr</a>.</p>
+<p class="has-small-font-size">Discuss this spotlight in the comments or on your favorite social media: <a href="#" rel="nofollow">instagram</a>, <a href="#" rel="nofollow">facebook</a>, <a href="#" rel="nofollow">reddit</a>, <a href="#" target="_blank" rel="noopener nofollow" title="">twitter</a>, <a href="#" target="_blank" rel="noopener nofollow" title="">mastodon</a>.</p>
 <!-- /wp:paragraph -->
 <!-- /wp:group -->
 
