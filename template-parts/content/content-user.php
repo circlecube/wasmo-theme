54bae2afcc61b8584e37b464dcb11d54b88d137e
Evan Mullins <evanrm@gmail.com>
2025-01-05 15:32:49 -0500

    add video embed for profiles

--- template-parts/content/content-user.php
+++ template-parts/content/content-user.php
@@ -24,6 +24,36 @@ $curauth = (get_query_var('author_name')) ? get_user_by('slug', get_query_var('a
 	?></div>
 <?php } ?>
 
+<?php if ( get_field( 'video', 'user_' . $userid ) ) { ?>
+	<div class="profile-video"><?php 
+		// Load value.
+		$iframe = get_field('video');
+
+		// Use preg_match to find iframe src.
+		preg_match('/src="(.+?)"/', $iframe, $matches);
+		$src = $matches[1];
+
+		// Add extra parameters to src and replace HTML.
+		$params = array(
+			'controls'  => 0,
+			'hd'        => 1,
+			'autohide'  => 1,
+			'autoplay'  => 0,
+			'loop'      => 0,
+			'rel'       => 0,
+		);
+		$new_src = add_query_arg($params, $src);
+		$iframe = str_replace($src, $new_src, $iframe);
+
+		// Add extra attributes to iframe HTML.
+		$attributes = 'frameborder="0"';
+		$iframe = str_replace('></iframe>', ' ' . $attributes . '></iframe>', $iframe);
+
+		// Display customized HTML.
+		echo $iframe;
+	?></div>
+<?php } ?>
+
 <?php 
 $shelf_items = get_field( 'my_shelf', 'user_' . $userid );
 if ( $shelf_items ) { ?>
